47e31ffbf5e6d3e827de0912d172d07cbe25dd42
Eric Huang <useforcode003@gmail.com>
2022-02-20 16:51:20 +0800

    golf

--- golf/golfrr.py
+++ golf/golfrr.py
@@ -1,17 +1,24 @@
-from pycat.core import Window, Sprite, Point
+from pycat.core import Window, Sprite, Point,Color
+from math import sqrt
 
 w = Window()
 
-class Line(Sprite):
+class Golf(Sprite):
 
     def on_create(self):
-        
-        self.line = w.create_line() 
-    
-    def on_update(self, dt):
-        a = Point(0,0)
-        b = w.mouse_position
-        self.line.set_start_end(a,b)
+        self.aim = w.create_line(width=6) 
+        self.scale = 26
+        self.color = Color.BLUE
+        self.position = (100,100)   
+        self.speed = 0
 
-line = w.create_sprite(Line)
+    def on_update(self, dt):
+        l = w.mouse_position - self.position
+        if l.x == 0 and l.y == 0:
+            self.aim.set_start_end(self.position,self.position)
+        else:
+            l.normalize()
+            self.aim.set_start_end(self.position,self.position + 100*l)
+
+golf = w.create_sprite(Golf)
 w.run()
